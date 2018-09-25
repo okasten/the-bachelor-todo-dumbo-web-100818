2152997c5bcda8c4782b1689322815a82e69cbf4
Olivia Kasten <odkasten@gmail.com>
2018-09-25 20:09:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,16 @@
 def get_first_name_of_season_winner(data, season)
-  
+  winner_name = ""
+  data.each do |seasons, contestant|
+    if seasons == season 
+      contestant.each do |description|
+        if description.values == "Winner"
+          winner_name = contestant
+        end 
+      end 
+    end
+  end 
+  winner_name.split(" ")
+  winner_name[0]
 end
 
 def get_contestant_name(data, occupation)
